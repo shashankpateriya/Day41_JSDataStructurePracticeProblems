d015fc8c0f3174c44e57762edd9316b52ed41eb7
Shashank <shashankpateriya4@gmail.com>
2021-08-24 23:48:02 +0530

    Add project files.

---
+++ RepeatedNumbers.js
@@ -0,0 +1,13 @@
+function numberIsRepeated(number){
+    if(number % 11 == 0)
+    return true;
+    else
+    return false;
+  }
+  
+  let array = new Array();
+  for(let i = 10; i < 100; i++){
+  if(numberIsRepeated(i)) array.push(i);
+  }
+  
+  console.log(array);
